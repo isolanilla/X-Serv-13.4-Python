100745aefa87e3fc794970ae3a04441c58f824b8
ismael solanilla prieto <isolanil@lambda18.aulas.gsyc.es>
2015-01-27 10:52:18 +0100

    creando script passwd

--- scriptpasswd.py
+++ scriptpasswd.py
@@ -2,7 +2,18 @@
 
 fd = open('/etc/passwd','r')
 lista = fd.readlines()
+dicc = {}
 
 for linea in lista:
 	lista2 = linea.split(':')
-	print "usuario:", lista2[0], "shell", lista2[len(lista2) -1]
+	
+	#print "usuario:", lista2[0], "shell", lista2[len(lista2) -1]
+	dicc[lista2[0]] = lista2[-1][:-1] # Para quitarme el \n 
+
+print dicc["root"]
+print len(dicc)
+
+try:
+	print dicc["imaginario"]
+except KeyError:
+	print "Usuario imaginario no existe"
